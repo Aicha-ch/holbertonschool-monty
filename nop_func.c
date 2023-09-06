b0e8b6f4b8e6a997e2a0eb382612fbac8dfa84a0
holbiwan <6188@holbertonstudents.com>
2023-09-06 07:00:34 -0700

    fix code after main function update

--- nop_func.c
+++ nop_func.c
@@ -1,11 +1,11 @@
+#include "monty.h"
+
 /**
  * _nop - Does nothing.
  * @stack: Pointer to the head of the stack.
  * @line_number: Line number in the Monty file.
  */
 
-void _nop(stack_t **stack, unsigned int line_number)
-{
-    (void)stack;
+void _nop(stack_t **stack, unsigned int line_number)    (void)stack;
     (void)line_number;
 }
