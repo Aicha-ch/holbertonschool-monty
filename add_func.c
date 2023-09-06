93b0a50fa213f6dbcfbc3d9a1fdc8de43852205c
holbiwan <6188@holbertonstudents.com>
2023-09-06 06:23:19 -0700

    fix code

---
+++ add_func.c
@@ -0,0 +1,18 @@
+/**
+ * _add - Adds the top two elements of the stack.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: Line number in the Monty file.
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n += (*stack)->n;
+    _pop(stack, line_number);
+}
