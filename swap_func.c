3577835a13f4781ca7b57b6beedab6ce9b1ee76d
holbiwan <6188@holbertonstudents.com>
2023-09-06 06:24:46 -0700

    fix code

---
+++ swap_func.c
@@ -0,0 +1,18 @@
+/**
+ * _swap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: Line number in the Monty file.
+ */
+
+void _swap(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
+}
