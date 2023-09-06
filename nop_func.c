af04fdcaae4200bcc1ecc48ba1d3f2c86882eaa6
holbiwan <6188@holbertonstudents.com>
2023-09-06 06:23:48 -0700

    nop_func.c

---
+++ nop_func.c
@@ -0,0 +1,11 @@
+/**
+ * _nop - Does nothing.
+ * @stack: Pointer to the head of the stack.
+ * @line_number: Line number in the Monty file.
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
